74cdf5b4b9e2e434355ea69a2d16172096e0ca24
vovasamokhvalov <vovasamokhvalov@mail.ru>
2023-08-18 02:24:36 +0300

    polychaetsya

--- scripts.js
+++ scripts.js
@@ -9,12 +9,35 @@ function myclick(){
     //console.log('Work');
     let a = document.querySelector('.City').value;
     console.log(a);
-    document.querySelector('.out').innerHTML = a;
+    document.querySelector('.out1').innerHTML = 'Your City: '+a;
 
-}
+
+let place_name = a
+console.log(place_name)
 
 const API_KEY_YANDEX = '99a52a8a-e301-4a65-a539-91d86dcb0109'
-const API_URL_GEO_DATA = `https://geocode-maps.yandex.ru/1.x/?apikey=API_KEY_YANDEX&geocode=Москва,+Тверская+улица,+дом+7&format=json`
+const API_URL_GEO_DATA = `https://geocode-maps.yandex.ru/1.x/?apikey=${API_KEY_YANDEX}&geocode=${place_name}&format=json`
 
+console.log(API_URL_GEO_DATA)
 
+fetch(API_URL_GEO_DATA)
+.then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    //console.log(data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos);
+    //let b = get(data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos);
+    document.querySelector('.out2').innerHTML = 'Координаты: '+data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
+  });
 
+  async function getcord() {
+    let cord = 0
+    let response = await fetch(API_URL_GEO_DATA);
+      let data = await response.json();
+      cord = data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
+      
+      console.log(cord);
+   }
+   getcord();
+
+}
